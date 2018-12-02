cc40b81f6c58676d85f361607dfedf91b977355b
Weiran Gu <weirangu@gmail.com>
2018-12-01 20:36:29 -0500

    Added opcode functions.

--- include/chip8/opcodes.h
+++ include/chip8/opcodes.h
@@ -1,6 +1,24 @@
 #ifndef CHIP_8_OPCODES_H
 #define CHIP_8_OPCODES_H
 
-void (*byte1[])(); // Stores function pointers for functions that will process the first byte
+// A list of pointers that hold functions that process opcodes
+// sig is the significant byte, insig is the insignificant byte
+void (*ops[0xF])(unsigned char sig, unsigned char insig);
 
+void x0(unsigned char sig, unsigned char insig);
+void x1(unsigned char sig, unsigned char insig);
+void x2(unsigned char sig, unsigned char insig);
+void x3(unsigned char sig, unsigned char insig);
+void x4(unsigned char sig, unsigned char insig);
+void x5(unsigned char sig, unsigned char insig);
+void x6(unsigned char sig, unsigned char insig);
+void x7(unsigned char sig, unsigned char insig);
+void x8(unsigned char sig, unsigned char insig);
+void x9(unsigned char sig, unsigned char insig);
+void xA(unsigned char sig, unsigned char insig);
+void xB(unsigned char sig, unsigned char insig);
+void xC(unsigned char sig, unsigned char insig);
+void xD(unsigned char sig, unsigned char insig);
+void xE(unsigned char sig, unsigned char insig);
+void xF(unsigned char sig, unsigned char insig);
 #endif
--- src/chip8/opcodes.c
+++ src/chip8/opcodes.c
@@ -1,22 +1,84 @@
 #include "chip8/opcodes.h"
 
-void (*byte1[])() = {
-    // 0x0
-    // 0x1
-    // 0x2
-    // 0x3
-    // 0x4
-    // 0x5
-    // 0x6
-    // 0x7
-    // 0x8
-    // 0x9
-    // 0xA
-    // 0xB
-    // 0xC
-    // 0xD
-    // 0xF
+void (*ops[0xF])(unsigned char sig, unsigned char insig) = {
+    x0, // 0x0
+    x1, // 0x1
+    x2, // 0x2
+    x3, // 0x3
+    x4, // 0x4
+    x5, // 0x5
+    x6, // 0x6
+    x7, // 0x7
+    x8, // 0x8
+    x9, // 0x9
+    xA, // 0xA
+    xB, // 0xB
+    xC, // 0xC
+    xD, // 0xD
+    xF  // 0xF
 };
 
+void x0(unsigned char sig, unsigned char insig){
+    // TODO
+}
 
+void x1(unsigned char sig, unsigned char insig){
+    // TODO
+}
+
+void x2(unsigned char sig, unsigned char insig){
+    // TODO
+}
+
+void x3(unsigned char sig, unsigned char insig){
+    // TODO
+}
+
+void x4(unsigned char sig, unsigned char insig){
+    // TODO
+}
+
+void x5(unsigned char sig, unsigned char insig){
+    // TODO
+}
+
+void x6(unsigned char sig, unsigned char insig){
+    // TODO
+}
+
+void x7(unsigned char sig, unsigned char insig){
+    // TODO
+}
+
+void x8(unsigned char sig, unsigned char insig){
+    // TODO
+}
+
+void x9(unsigned char sig, unsigned char insig){
+    // TODO
+}
+
+void xA(unsigned char sig, unsigned char insig){
+    // TODO
+}
+
+void xB(unsigned char sig, unsigned char insig){
+    // TODO
+}
+
+void xC(unsigned char sig, unsigned char insig){
+    // TODO
+}
+
+void xD(unsigned char sig, unsigned char insig){
+    // TODO
+}
+
+void xE(unsigned char sig, unsigned char insig){
+    // TODO
+}
+
+void xF(unsigned char sig, unsigned char insig){
+    // TODO
+}
 
