efa59e5b8c29d00f4a91d65b0e54706e204b577d
Weiran Gu <weirangu@gmail.com>
2018-12-21 14:34:41 -0500

    Added decrement timer code.

--- src/chip8/timers.c
+++ src/chip8/timers.c
@@ -2,3 +2,15 @@
 
 void init_timers(chip8_timers* timer){
 }
+
+void decrement_timer(chip8_timers* timer, void (*sound) ()){
+    if (timer->delay_timer > 0){
+        timer->delay_timer--;
+    }
+    if (timer->sound_timer > 0){
+        timer->sound_timer--;
+        if (timer->sound_timer == 0){
+            sound();
+        }
+    }
+}
